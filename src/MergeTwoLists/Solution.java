ed7155134337ec3ad8177c57b0236c47d98ce897
Hiten1928 <hg1928@rit.edu>
2018-06-20 23:55:05 -0400

    merge two sorted lists

---
+++ src/MergeTwoLists/Solution.java
@@ -0,0 +1,29 @@
+package MergeTwoLists;
+
+/**
+ * Definition for singly-linked list.
+ * public class MergeTwoLists.ListNode {
+ *     int val;
+ *     MergeTwoLists.ListNode next;
+ *     MergeTwoLists.ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+
+        if(l1.val<l2.val)
+        {
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        }
+        else
+        {
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+
+    }
+    
+}
