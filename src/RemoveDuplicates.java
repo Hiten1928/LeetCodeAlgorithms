d77dab11538c6332a1a4a74569c290d50490b18b
Hiten1928 <hg1928@rit.edu>
2018-06-21 11:02:55 -0400

    remove duplicate elements from an array

--- src/RemoveDuplicates.java
+++ src/RemoveDuplicates.java
@@ -1,2 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class RemoveDuplicates {
+    public static int removeDuplicates(int[] nums) {
+        if(nums.length == 0)
+            return nums.length;
+        int read = 0;
+        for(int write=0; write<nums.length; write++)
+        {
+            if (nums[write] != nums[read])
+            {
+                read++;
+                nums[read] = nums[write];
+            }
+        }
+        return read+1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] nums = stringToIntegerArray(line);
+
+            int ret = removeDuplicates(nums);
+            String out = integerArrayToString(nums, ret);
+
+            System.out.print(out);
+        }
+    }
+
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static String integerArrayToString(int[] nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+
+        String result = "";
+        for(int index = 0; index < length; index++) {
+            int number = nums[index];
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static String integerArrayToString(int[] nums) {
+        return integerArrayToString(nums, nums.length);
+    }
+
+
 }
