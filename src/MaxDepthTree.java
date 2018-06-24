bb6dc933b090aaae4d84be7d0bd4f8f015d36e40
Hiten1928 <hg1928@rit.edu>
2018-06-24 18:51:14 -0400

    adding function for tree

--- src/MaxDepthTree.java
+++ src/MaxDepthTree.java
@@ -1,2 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
 public class MaxDepthTree {
+    public static int maxDepth(BinaryTreeNode root) {
+        if(root ==null){
+            return 0;
+        } else{
+            int leftLength = maxDepth(root.left);
+            int rightLength = maxDepth(root.right);
+            int maxHeight = Math.max(leftLength,rightLength) + 1;
+            return maxHeight;
+        }
+
+    }
+
+
 }
