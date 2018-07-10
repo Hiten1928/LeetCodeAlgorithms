03c5116659597462388abf052b792e4a93652517
Hiten1928 <hg1928@rit.edu>
2018-07-10 12:14:58 -0400

    Anagram

---
+++ src/Anagram.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    
+    var flag new Boolean(false);
+    
+    s =s.split('').sort().join('');
+    t =t.split('').sort().join('');
+    
+    for(let char of t){
+        for(let char1 of s){
+            if(char == char1){
+                flag = true;
+            }
+        }
+    }
+    
+    return flag;
+};
---
+++ src/HaaminWeight.java
@@ -0,0 +1,10 @@
+
+
+//        Input: 11
+//        Output: 3
+//        Explanation: Integer 11 has binary representation 00000000000000000000000000001011
+
+
+
+public class HaaminWeight {
+}
