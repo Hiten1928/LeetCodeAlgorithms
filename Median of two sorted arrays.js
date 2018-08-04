6358041d123d3840738ad63259dfa6b2728404dc
Hiten1928 <31774329+Hiten1928@users.noreply.github.com>
2018-08-04 15:05:47 -0400

    Median of arrays - optimize attempt perfecto 100%

--- Median of two sorted arrays.js
+++ Median of two sorted arrays.js
@@ -26,12 +26,27 @@ The median is (2 + 3)/2 = 2.5
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    var arr =nums1.concat(nums2).sort((a,b) => a-b);
+    var arr =[];
+    if(nums1.length === 0 && nums2.length === 0){
+        return 0.0;
+    }
+    if(nums1.length === 0){
+        arr = nums2;
+    } else if(nums2.length === 0){
+        arr = nums1;
+    }else{
+    arr = nums1.concat(nums2);
+    }
+    arr = arr.sort((a,b) => parseInt(a)-parseInt(b));
     var length = arr.length;
     var len = length % 2;
-    if(len === 0){
-        return (arr[Math.floor(length/2)] + arr[Math.floor(length/2)-1])/2;
+    if(arr.length === 1){
+        return parseInt(arr[0]);
+    }else
+    if(len !== 0 && arr.length > 1){
+        return parseInt(arr[Math.floor(length/2)]);
     } else {
-       return arr[Math.floor(length/2)];
+       return ((parseInt(arr[Math.floor(length/2)]) + parseInt(arr[Math.floor(length/2)-1]))/2);
     }
+    
 };
